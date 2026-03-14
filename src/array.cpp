f39cce60a09e7f853cca3a2e6bae7739c9a347bd
Joebert Cerezo <joebertcerezo@gmail.com>
2026-03-14 19:20:59 +0800

    feat: add initial implementation of array example in array.cpp

---
+++ src/array.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+  cout << "Hello world";
+
+  int n[10];
+
+  for (int i = 0; i < 10; i++) {
+    n[i] = i + 100;
+  }
+
+  return 0;
+}
