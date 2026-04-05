40c49bef93d1f11f5fc8e60e6a4498db8e7e650d
Joebert Cerezo <joebertcerezo@gmail.com>
2026-04-05 22:44:15 +0800

    feat: enhance pointers and references demonstration with additional examples

--- src/03_pointers_refs.cpp
+++ src/03_pointers_refs.cpp
@@ -1,11 +1,28 @@
 #include <iostream>
+#include "maTry.h"
 
 using namespace std;
 
 // This file covers pointers and references, a core concept in C++.
 
+void passByValue(int x);
+void passByReference(int &x);
+void passByPointer(int *x);
+
 int main() {
-  cout << "--- Pointers & References ---\n";
+  int num1 = 1;
+  int &tryRef = num1;
+  int *pp = &tryRef;
+
+  cout << tryRef << "\n";
+  cout << &tryRef << "\n";
+
+  cout << "\n";
+  cout << pp << "\n";
+  cout << *pp << "\n";
+
+
+  cout << "\n\n--- Pointers & References ---\n";
 
   int score = 100;
 
@@ -30,5 +47,33 @@ int main() {
     cout << "This pointer doesn't point to anything yet.\n";
   }
 
+  int a = 5;
+  cout << "a is 5 \n";
+
+  cout << "passByValue(a)\n";
+  passByValue(a);
+  cout << "new value of a is " << a << "\n";
+
+  cout << "passByReference(a)\n";
+  passByReference(a);
+  cout << "new value of a is " << a << "\n";
+
+  cout << "passByPointer(a)\n";
+  passByPointer(&a);
+  cout << "new value of a is " << a << "\n";
+  printMe();
+
   return 0;
+}
+
+void passByValue(int x){
+  x = 10;
+}
+
+void passByReference(int &x){
+  x = 15;
+}
+
+void passByPointer(int *x){
+  *x = 20;
 }
