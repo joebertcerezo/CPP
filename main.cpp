e8db51af9b53fac0c5b4aae48ca4f7b3a3b08066
Joebert Cerezo <joebertcerezo@gmail.com>
2026-05-09 12:32:08 +0800

    feat: add initial main.cpp file with Hello World implementation

---
+++ main.cpp
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/cppFiles/main.cc to edit this template
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: jlcerezo
+ *
+ * Created on May 9, 2026, 12:20 PM
+ */
+
+#include <cstdlib>
+#include <iostream>
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    cout << "Hello world";
+    return 0;
+}
+
